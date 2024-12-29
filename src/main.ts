b20dcd570233c3dd5df562fac193af56dae05947
Nils Ingwersen <andresennils68@gmail.com>
2024-12-29 18:01:47 +0100

    feat: added 3d globe with styling

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,22 @@
+import { Map, type StyleSpecification } from "maplibre-gl";
+
+import style from "./styles/mapstyle.json";
+
 import "./style.css";
 
-const app = document.querySelector<HTMLDivElement>("#app")!;
+const mapDiv = document.querySelector<HTMLDivElement>("#map")!;
+
+const map = new Map({
+  container: mapDiv,
+  style: style as StyleSpecification,
+  center: [10, 53], // starting position [lng, lat]
+  zoom: 3,
+  hash: true,
+  attributionControl: false,
+});
+
+map.on("style.load", () => {
+  map.setProjection({
+    type: "globe", // Set projection to globe
+  });
+});
